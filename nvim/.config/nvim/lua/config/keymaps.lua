148d67b940fd093214817b3e5327a03da691f1e4
Blair Rush <blair.t.rush@gmail.com>
2025-08-26 01:17:40 -0500

    fixing stuff up more

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -6,6 +6,9 @@ vim.keymap.set("n", "<C-h>", "<C-w><left>", {})
 vim.keymap.set("n", "<C-j>", "<C-w><down>", {})
 vim.keymap.set("n", "<C-k>", "<C-w><up>", {})
 
+local sessions = require('mini.sessions')
+vim.keymap.set("n", "<leader>ss", function() sessions.write() end, { desc = "Save session" })
+
 local noice = require('noice')
 vim.keymap.set("n", "<leader>n", ':Noice history<CR>', { desc = "Notification history" })
 
@@ -27,16 +30,16 @@ vim.keymap.set("n", "gI", function() vim.lsp.buf.implementation() end, { desc = 
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<C-p>', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>fc', function() builtin.find_files({ cwd = '~/.config/nvim/' }) end,
-  { desc = 'Telescope find files' })
+	{ desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>/', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 vim.keymap.set('n', '<leader>fp', ":lua require'telescope'.extensions.project.project{ hidden=true }<CR>",
-  { desc = 'Telescope projects' })
+	{ desc = 'Telescope projects' })
 vim.keymap.set('n', '<leader>fg', ":lua require'telescope'.extensions.repo.repo{}<CR>",
-  { desc = 'Telescope git projects' })
+	{ desc = 'Telescope git projects' })
 vim.keymap.set('n', '<leader>fr', ":lua require'telescope'.extensions.frecency.frecency{workspace=CWD}<CR>",
-  { desc = 'Telescope git projects' })
+	{ desc = 'Telescope git projects' })
 
 vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -1,9 +1,11 @@
 return {
-  'echasnovski/mini.nvim', version = false,
+  'echasnovski/mini.nvim',
+  version = false,
   config = function()
     require('mini.ai').setup()
     require('mini.bracketed').setup()
     require('mini.pick').setup()
     require('mini.surround').setup()
+    require('mini.sessions').setup()
   end
 }
