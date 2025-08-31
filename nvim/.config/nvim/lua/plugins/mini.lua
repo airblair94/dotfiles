f4e9780ba7921e9b2cc3b80c2914ef30f626904a
Blair Rush <blair.t.rush@gmail.com>
2025-08-31 03:12:33 -0500

    fixing mini ai

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -2,7 +2,20 @@ return {
   'echasnovski/mini.nvim',
   version = false,
   config = function()
-    require('mini.ai').setup()
+    local gen_spec = require('mini.ai').gen_spec
+    require('mini.ai').setup({
+      n_lines = 500,
+      custom_textobjects = {
+        a = gen_spec.treesitter { a = '@parameter.outer', i = '@parameter.inner' },
+        c = gen_spec.treesitter { a = '@class.outer', i = '@class.inner' },
+        f = gen_spec.treesitter { a = '@function.outer', i = '@function.inner' },
+        F = gen_spec.treesitter { a = '@call.outer', i = '@call.inner' },
+        o = gen_spec.treesitter {
+          a = { '@block.outer', '@conditional.outer', '@loop.outer' },
+          i = { '@block.inner', '@conditional.inner', '@loop.inner' },
+        },
+      },
+    })
     require('mini.bracketed').setup()
     require('mini.pick').setup()
     require('mini.surround').setup()
